277209dc72c774ae5003787d899d51468189e463
Jan <kupper.thorge@gmail.com>
2025-08-17 16:30:58 +0200

    refactor layout and improve structure for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { ArrowRight, ArrowUpRight } from "lucide-react";
+import { ArrowUpRight } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
@@ -38,29 +38,48 @@ export default function Home() {
   ];
 
   return (
-    <div className="flex h-full w-full items-center justify-center bg-white">
-      <div className="w-full max-w-4xl text-2xl">
-        {pages.map((page, i) => (
-          <div className="my-4 flex items-center justify-between gap-4" key={i}>
-            <span className="">{page.day < 10 ? `0${page.day}` : page.day} </span>
-            <Link className="group" href={`/day${page.day}`}>
-              <div className="mx-auto flex w-fit items-center gap-2 bg-black px-3 py-1 text-white hover:bg-black/80">
-                {page.title}
-                <ArrowRight className="transition-transform group-hover:translate-x-1" />
+    <div className="grid h-full w-full grid-cols-1 gap-2 p-2 text-[clamp(14px,2vw,24px)] lg:grid-cols-2 lg:gap-4 lg:p-4">
+      <div className="w-full border border-black/10 p-4">
+        <div className="flex h-full max-w-1/2 flex-col justify-between">
+          <h1 className="text-[clamp(22px,3.5vw,40px)] leading-tight font-medium">
+            Daily Creative Design Exercise
+          </h1>
+          <p className="text-[clamp(12px,1.5vw,20px)] leading-tight">
+            I recreate components from Awwwards winners to develop advanced frontend animation and
+            interaction skills using GSAP and creative coding techniques.
+          </p>
+        </div>
+      </div>
+      <div className="flex w-full items-end border border-black/10 p-4">
+        <ul className="w-full">
+          {pages.map((page, i) => (
+            <li key={page.day}>
+              <div className="flex">
+                <Link
+                  href={`/day${page.day}`}
+                  className="w-full transition-opacity hover:opacity-50"
+                >
+                  <div className="flex w-full">
+                    <p className="w-1/5">
+                      {page.day < 10 && "0"}
+                      {page.day}
+                    </p>
+                    <p className="w-3/5">{page.title}</p>
+                  </div>
+                </Link>
+                <a
+                  target="_blank"
+                  href={page.sodt.url}
+                  className="group flex items-center px-1 transition-opacity hover:opacity-50"
+                >
+                  src{" "}
+                  <ArrowUpRight className="transition-transform group-hover:translate-x-0.5 group-hover:-translate-y-0.5" />
+                </a>
               </div>
-            </Link>
-            <div className="flex-1">
-              <a
-                className="group ml-auto flex w-fit items-center gap-1 px-3 py-1 text-right hover:bg-black hover:text-white"
-                target="_blank"
-                href={page.sodt.url}
-              >
-                {page.sodt.name}
-                <ArrowUpRight className="transition-transform group-hover:translate-x-0.5 group-hover:-translate-y-0.5" />
-              </a>
-            </div>
-          </div>
-        ))}
+              {i != pages.length - 1 && <div className="my-1 h-[1px] bg-black/10"></div>}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
